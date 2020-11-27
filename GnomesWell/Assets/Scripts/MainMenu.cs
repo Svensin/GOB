2ef7b16081024ed69918912f37edf4abadf6f60f
Svensin <mfarkovecz@gmail.com>
2020-11-27 15:41:10 +0200

    Added Gnomes Well project

---
+++ GnomesWell/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// BEGIN 2d_mainmenu
+using UnityEngine.SceneManagement;
+
+// Manages the main menu.
+public class MainMenu : MonoBehaviour {
+
+	public GameObject mainMenu;
+	public GameObject settingsMenu;
+	public GameObject chooseLevelMenu;
+	public GameObject exitWarningMenu;
+
+	public GameObject musicButton;
+	public Sprite musicButtonOnSprite;
+	public Sprite musicButtonOffSprite;
+
+	public GameObject musicIcon;
+	public Sprite musicIconOnSprite;
+	public Sprite musicIconOffSprite;
+
+	public GameObject adsButton;
+	public Sprite adsButtonOnSprite;
+	public Sprite adsButtonOffSprite;
+
+	public GameObject adsIcon;
+	public Sprite adsIconOnSprite;
+	public Sprite adsIconOffSprite;
+
+
+	GameObject currentMenu;
+
+	bool isMusicOn = true;
+	bool isAdsOn = true;
+
+	Image musicButtonImage;
+	Image musicIconImage;
+	Image adsButtonImage;
+	Image adsIconImage;
+
+	public void Start() {
+		currentMenu = mainMenu;
+		currentMenu.SetActive(true);
+		exitWarningMenu.SetActive(false);
+		settingsMenu.SetActive(false);
+		chooseLevelMenu.SetActive(false);
+
+	musicButtonImage = musicButton.GetComponent<Image>();
+		musicIconImage = musicIcon.GetComponent<Image>();
+		adsButtonImage = adsButton.GetComponent<Image>();
+		adsIconImage = adsIcon.GetComponent<Image>();
+	}
+
+	private void Update()
+	{
+		if (mainMenu.activeInHierarchy == true && (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Menu)))
+		{
+			ActivateExitWarningMenu();
+		}
+	}
+
+    public void BackToMainMenu()
+    {
+		currentMenu.SetActive(false);
+		mainMenu.SetActive(true);
+		currentMenu = mainMenu;
+    }
+
+	public void ToSettingsMenu()
+	{
+		currentMenu.SetActive(false);
+		settingsMenu.SetActive(true);
+		currentMenu = settingsMenu;
+	}
+
+	public void ToChooseLevelMenu()
+	{
+		currentMenu.SetActive(false);
+		chooseLevelMenu.SetActive(true);
+		currentMenu = chooseLevelMenu;
+	}
+
+	public void ChangeMusicState()
+    {
+		isMusicOn = !isMusicOn;
+
+		if (isMusicOn == true)
+        {
+			musicButtonImage.sprite = musicButtonOnSprite;
+			musicIconImage.sprite = musicIconOnSprite;
+        }
+        else
+        {
+			musicButtonImage.sprite = musicButtonOffSprite;
+			musicIconImage.sprite = musicIconOffSprite;
+		}
+    }
+
+	public void ChangeAdsState()
+	{
+		isAdsOn = !isAdsOn;
+
+		if (isAdsOn == true)
+		{
+			adsButtonImage.sprite = adsButtonOnSprite;
+			adsIconImage.sprite = adsIconOnSprite;
+		}
+		else
+		{
+			adsButtonImage.sprite = adsButtonOffSprite;
+			adsIconImage.sprite = adsIconOffSprite;
+		}
+	}
+
+	public void LoadLevel(int id)
+    {
+		SceneManager.LoadScene(id);
+    }
+
+
+	public void ActivateExitWarningMenu()
+    {
+		exitWarningMenu.SetActive(true);
+    }
+
+	public void DisactivateExitWarningMenu()
+    {
+		exitWarningMenu.SetActive(false);
+	}
+
+	public void QuitApplication()
+    {
+		Application.Quit();
+    }
+}
+// END 2d_mainmenu
---
+++ GnomesWell/Assets/Scripts/SignalOnTouch.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// BEGIN 2d_signalontouch
+using UnityEngine.Events;
+
+// Invokes a UnityEvent when the Player collides with this object.
+[RequireComponent (typeof(Collider2D))]
+public class SignalOnTouch : MonoBehaviour {
+
+	// The UnityEvent to run when we collide.
+	// Attach methods to run in the editor.
+	public UnityEvent onTouch;
+
+	public GameObject gameManager;
+
+	string objectThatHitName;
+
+	// If true, attempt to play an AudioSource when we collide.
+	public bool playAudioOnTouch = true;
+
+	// When we enter a trigger area, call SendSignal.
+	void OnTriggerEnter2D(Collider2D collider) {
+		SendSignal (collider.gameObject);
+	}
+
+	// When we collide with this object, call SendSignal.
+	void OnCollisionEnter2D(Collision2D collision) {
+		SendSignal (collision.gameObject);
+	}
+
+	// Checks to see if this object was tagged as Player, and invoke
+	// the UnityEvent if it was.
+	void SendSignal(GameObject objectThatHit) {
+
+		if (gameObject.CompareTag("Collectable") == true)
+		{
+			if (objectThatHit.CompareTag("Player") == true)
+			{
+				if (gameObject.name.CompareTo("Dagger teleport") == 0)
+                {
+					gameObject.GetComponent<SpriteRenderer>().enabled = false;
+					gameObject.GetComponent<Collider2D>().enabled = false;
+				}
+
+				onTouch.Invoke();
+			}			
+		}
+		else 
+		{
+			// Was this object tagged Player?
+			if (objectThatHit.CompareTag("Player") == true)
+			{
+
+				gameManager.GetComponent<GameManager>().objectThatHitName = objectThatHit.name;
+
+				// If we should play a sound, attempt to play it
+				if (playAudioOnTouch)
+				{
+					var audio = GetComponent<AudioSource>();
+
+					if (audio && audio.gameObject.activeInHierarchy)
+						audio.Play();
+				}
+
+
+				// Invoke the event
+				onTouch.Invoke();
+			}
+		}	
+	}
+
+	
+}
+// END 2d_signalontouch
